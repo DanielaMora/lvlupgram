b12bea8e4ec0bcea50614197b1cb1ab298dbee59
Daniela <danielalvlupcamp@gmail.com>
2016-07-29 21:29:13 +0000

    fonts, colors, style

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -10,5 +10,4 @@ class Topic < ActiveRecord::Base
 
     # Validate the attached image is image/jpg, image/png, etc
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-
-end
+  end
