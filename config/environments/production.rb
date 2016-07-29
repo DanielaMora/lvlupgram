587c129ee7c5fcdade8edd66de0daf5320efa724
Daniela <danielalvlupcamp@gmail.com>
2016-07-29 18:43:05 +0000

    format

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,8 +75,7 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
- config.paperclip_defaults = {
+  config.paperclip_defaults = {
   storage: :s3,
    s3_credentials: {
      bucket: ENV.fetch('S3_BUCKET_NAME'),
@@ -84,6 +83,7 @@ Rails.application.configure do
      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
      s3_region: ENV.fetch('AWS_REGION')
    }
-  }
+ }
+
 
 end
